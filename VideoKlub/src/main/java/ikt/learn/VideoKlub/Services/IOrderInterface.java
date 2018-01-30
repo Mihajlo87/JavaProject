60a18adf3096cb6533e90ab26c4cd9d88517cd18
Mihajlo87 <mihajlo.stankov87@gmail.com>
2018-01-30 09:33:34 +0100

    das
    dasd

--- VideoKlub/src/main/java/ikt/learn/VideoKlub/Services/IOrderInterface.java
+++
@@ -1,9 +0,0 @@
-package ikt.learn.VideoKlub.Services;
-
-import ikt.learn.VideoKlub.Exception.TotalCountException;
-import ikt.learn.VideoKlub.Filter.OrderFilter;
-
-public interface IOrderInterface {
-
-	public Long updateOrder(OrderFilter filter) throws TotalCountException;
-}
